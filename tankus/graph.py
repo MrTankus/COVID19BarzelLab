aa156166e5c7c2b14eafac3c4a757a8842a0ed80
Gil Tankus <gtankus@akamai.com>
2020-04-09 21:53:28 +0300

    tankus commit

--- tankus/graph.py
+++ tankus/graph.py
@@ -2,6 +2,8 @@ import itertools
 import numpy as np
 from scipy.integrate import odeint
 
+import settings
+
 
 class State(object):
     RED = 0
@@ -11,15 +13,16 @@ class State(object):
 
 class ZonePopulation(object):
 
-    def __init__(self, susceptible, exposed, infected, recovered):
+    def __init__(self, susceptible, exposed, infected, recovered, hospitalized):
         self.susceptible = susceptible
         self.exposed = exposed
         self.infected = infected
+        self.hospitalized = hospitalized
         self.recovered = recovered
         self.total = susceptible + exposed + infected + recovered
 
     def calculate_positive_fraction(self):
-        return (self.exposed + self.infected) / self.total
+        return (self.exposed + self.infected + self.hospitalizedx) / self.total
 
 
 class MigrationPopulation(object):
@@ -27,6 +30,12 @@ class MigrationPopulation(object):
     def __init__(self, fraction):
         assert 0 < fraction < 1
         self.fraction = fraction
+
+    @classmethod
+    def calculate_migration_coefficient(cls, from_zone, to_zone, all_zones):
+        r = from_zone.distance_from(to_zone)
+        rhs = from_zone.population.total * sum([zone.population.total * r ** -2 for zone in filter(lambda z: z != from_zone, all_zones)])
+        return from_zone.population.total * settings.MIGRATION_COEFICIENT_PERCENTAGE / rhs
 
     def migrate_population(self, from_population, to_population):
 
@@ -38,16 +47,19 @@ class MigrationPopulation(object):
         migrated_susceptible = normalize(from_population.susceptible)
         migrated_exposed = normalize(from_population.exposed)
         migrated_infected = 0
+        migrated_hospitalized = 0
         migrated_recovered = normalize(from_population.recovered)
 
         to_population.susceptible += migrated_susceptible
         to_population.exposed += migrated_exposed
         to_population.infected += migrated_infected
+        to_population.hospitalized += migrated_hospitalized
         to_population.recovered += migrated_recovered
 
         from_population.susceptible -= migrated_susceptible
         from_population.exposed -= migrated_exposed
         from_population.infected -= migrated_infected
+        from_population.hospitalized -= migrated_hospitalized
         from_population.recovered -= migrated_recovered
 
 
@@ -70,29 +82,33 @@ class Zone(object):
     def distance_from(self, vertex):
         return np.sqrt((self.location[0] - vertex.location[0]) ** 2 + (self.location[1] - vertex.location[1]) ** 2)
 
-    def run_internal_dynamics(self, alpha, beta, gamma):
+    def run_internal_dynamics(self, alpha, beta, gamma, delta):
 
-        def model(z, t, a, b, g, population):
+        def model(z, t, a, b, g, d, population):
             s = z[0]
             e = z[1]
             i = z[2]
+            h = z[3]
             dsdt = -b * s * (e / population)
             dedt = b * e * (s / population) - g * e
             didt = g * e - a * i
-            drdt = a * i
-            return [dsdt, dedt, didt, drdt]
+            dhdt = d * i - a * h
+            drdt = a * (i + h)
+            return [dsdt, dedt, didt, dhdt, drdt]
 
-        t = np.linspace(8, 17, (17 - 8) * 60)
-        state = [self.population.susceptible, self.population.exposed, self.population.infected, self.population.recovered]
-        z = odeint(model, state, t, args=(alpha, beta, gamma, self.population.total))
+        t = np.linspace(0, 9, 10 * 60)
+        state = [self.population.susceptible, self.population.exposed, self.population.infected, self.population.hospitalized, self.population.recovered]
+        z = odeint(model, state, t, args=(alpha, beta, gamma, delta, self.population.total))
         susceptible_over_time = z[:, 0]
         exposed_over_time = z[:, 1]
         infected_over_time = z[:, 2]
+        hospitalzied_over_time = z[:, 2]
         recovered_over_time = z[:, 3]
 
         self.population.susceptible = susceptible_over_time[-1]
         self.population.exposed = exposed_over_time[-1]
         self.population.infected = infected_over_time[-1]
+        self.population.hospitalized = hospitalzied_over_time[-1]
         self.population.recovered = recovered_over_time[-1]
 
     def set_state(self, eta):
@@ -120,9 +136,6 @@ class Network(object):
         self.vertices = set(vertices)
         self.edges = set(edges)
         self.vertices_map = dict([(v.id, v) for v in self.vertices])
-        self.alpha = 0.1
-        self.beta = 0.5
-        self.gamma = 0.2
         self.eta = eta
 
     @classmethod
@@ -141,7 +154,7 @@ class Network(object):
             # TODO - randomize local initial state.
 
             population = ZonePopulation(susceptible=zone_population - 1, exposed=1,
-                                        infected=0, recovered=0)
+                                        infected=0, hospitalized=0, recovered=0)
             zone = Zone(id=zone, location=(i, j), population=population, state=State.GREEN)
             j += 1
             if j % 3 == 0:
@@ -149,14 +162,17 @@ class Network(object):
                 j = 0
             vertices.add(zone)
         edges = itertools.combinations(vertices, 2)
-        edges = set([Edge(v1=p[0], v2=p[1], weight=p[0].distance_from(p[1]) ** -2) for p in edges])
+
+        edges = set([Edge(v1=p[0], v2=p[1], weight=((p[0].population.total * p[1].population.total) * (p[0].distance_from(p[1]) ** -2))) for p in edges])
 
         for from_zone in vertices:
             from_zone_edges = filter(lambda e: e.is_on_edge(from_zone), edges)
             for link in from_zone_edges:
-                if np.random.random() < link.weight:
-                    to_zone = link.v1 if from_zone != link.v1 else link.v2
-                    migration_population = MigrationPopulation(fraction=0.1)
+                to_zone = link.v1 if from_zone != link.v1 else link.v2
+                c = MigrationPopulation.calculate_migration_coefficient(from_zone=from_zone, to_zone=to_zone, all_zones=vertices)
+                # TODO - fix this! link.weight is now c * (p1 * p2 / r^2) and not (1/r^2)
+                if np.random.random() < c * link.weight:
+                    migration_population = MigrationPopulation(fraction=0.01)
                     from_zone.migration_to_map[to_zone.id] = migration_population
                     to_zone.migration_from_map[from_zone.id] = migration_population
 
@@ -170,9 +186,9 @@ class Network(object):
         for zone in self.vertices:
             if zone.state is not None:
                 if zone.state != State.RED:
-                    zone.run_internal_dynamics(alpha=self.alpha, beta=self.beta, gamma=self.gamma)
+                    zone.run_internal_dynamics(alpha=settings.ALPHA, beta=settings.BETA, gamma=settings.GAMMA, delta=settings.DELTA)
                 else:
-                    zone.run_internal_dynamics(alpha=self.alpha, beta=0, gamma=self.gamma)
+                    zone.run_internal_dynamics(alpha=settings.ALPHA, beta=0, gamma=settings.GAMMA, delta=settings.DELTA)
         for zone in self.vertices:
             self.migrate_back(zone)
 
--- tankus/simulator.py
+++ tankus/simulator.py
@@ -8,6 +8,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
+import settings
+
 formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 handler = logging.StreamHandler(stdout)
 handler.setFormatter(formatter)
@@ -19,24 +21,24 @@ logger.addHandler(handler)
 def main():
     population = 9e6
     ventilators = 2e3
-    k = 0.1
     days = np.linspace(0, 364, 365, endpoint=True)
-    i_peak = np.array([ventilators / k for _ in days])
-    etas = [0.01, 0.001]
-    for eta in etas:
+    i_peak = np.array([ventilators for _ in days])
+    for eta in settings.ETAS:
         logger.info("initializing network with eta {}".format(eta))
         network = Network.initialize(zones=260, total_population=population, eta=eta)
         infectious = list()
         for day in days:
             logger.info("starting day {}".format(day))
-            infectious.append(sum(map(lambda zone: zone.population.infected, network.vertices)))
+            infectious.append(sum(map(lambda zone: zone.population.hospitalized, network.vertices)))
             network.increment()
         logger.info("objects in mem {}".format(gc.get_count()))
         gc.collect()
         plt.plot(days, infectious)
 
     plt.plot(days, i_peak)
-    plt.legend(['infectious over days - 0.01', 'infectious over days - 0.001', 'I peak'])
+    legends = ['H(t); eta = {}'.format(eta) for eta in etas]
+    legends.append('I peak')
+    plt.legend(legends)
     plt.show()
 
 
